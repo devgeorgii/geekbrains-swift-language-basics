5d7d245db68159588f5ac0ed83c88bc398b02543
Georgii Akhalaia <git@georgii.ge>
2024-08-12 16:50:48 +0400

    feat: add solution to problem 2 from lesson 2
    see the README for more information

--- 2l_GeorgiiAkhalaia.playground/Contents.swift
+++ 2l_GeorgiiAkhalaia.playground/Contents.swift
@@ -7,3 +7,11 @@ func isEvenNumber(_ number: Int) -> Bool {
 
 let a = 84
 isEvenNumber(a)
+
+// 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+func isDivisibleByThree(_ number: Int) -> Bool {
+    return number % 3 == 0
+}
+
+let b = 99
+isDivisibleByThree(b)
