0cf01b8e2d82efc3a2b5afe60cd91b73141b3737
Georgii Akhalaia <git@georgii.ge>
2024-08-12 17:48:17 +0400

    feat: add solution to problem 5 from lesson 2
    see the README for more information

--- 2l_GeorgiiAkhalaia.playground/Contents.swift
+++ 2l_GeorgiiAkhalaia.playground/Contents.swift
@@ -29,3 +29,35 @@ for count in array {
 }
 
 print("Задача №4:\n\(array)\n")
+
+// 5. (необязательно) Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 50 элементов. Числа Фибоначчи определяются соотношениями Fn=Fn-1 + Fn-2.
+
+/// Функция, которая добавляет в массив новое число Фибоначчи
+/// - Parameters:
+///   - array: Массив в который добавляем числа
+///   - howManyNumbersToAdd: Сколько чисел добавить
+///   - ifNotEmptyErase: Стереть массив, если в нём уже есть числа. Добавил эту функцию, так как в задании не уточняется начинать массив с [0, 1] как это обычно принято, или использовать уже доступные нам числа в массиве. Значение true - удаляет все числа из массива и заменяет их на [0, 1]. false - использует уже заданные числа.
+func addFibonacciNumbers(array: [Int], howManyNumbersToAdd: Int, ifNotEmptyErase: Bool) -> [Int] {
+    var functionArray: [Int] = array
+    var eraseStatus: Bool = ifNotEmptyErase
+    for count in 0..<howManyNumbersToAdd {
+        if functionArray.count == 0 { // Если массив пустой, добавляем [0, 1]
+            functionArray.append(contentsOf: [0, 1])
+        } else if functionArray.count != 0 && eraseStatus == true { // Если в массиве уже есть числа и мы хотим стереть массив и установить [0, 1]
+            functionArray.removeAll()
+            functionArray.append(contentsOf: [0, 1])
+            eraseStatus = false
+        } else if functionArray.count == 1 { // Если в массиве есть одно число и мы не хотим стереть его. Добавляем 1 в массив
+            functionArray.append(1)
+        } else { // Если в массиве есть 2 числа или больше. Складываем последний и предпоследний элемент массива, и добавляем результат в массив
+            let fn1 = functionArray.last
+            let fn2 = functionArray[functionArray.count - 2]
+            functionArray.append(fn1! + fn2)
+        }
+    }
+    return functionArray
+}
+
+var fibonacciArray: [Int] = [1, 8]
+fibonacciArray = addFibonacciNumbers(array: fibonacciArray, howManyNumbersToAdd: 50, ifNotEmptyErase: true)
+print("Задача №5:\n\(fibonacciArray)\n")
